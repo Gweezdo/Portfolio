159bfc7f66c12f820e56e5ea8649460b4015daeb
Gweezdo <z.trivium@gmail.com>
2020-05-20 21:53:33 +1000

    Star Animation finalised Navbar underline animation finalised Started adding start to index-main.html

--- main.js
+++ main.js
@@ -6,59 +6,64 @@
      NAVBAR ANIMATIONS
 *******************************/
 
-var navLinks = document.getElementsByClassName("nav-link");
+var navLinks = document.querySelectorAll(".nav-link");
 var underlines = document.querySelectorAll(".underline");
 
 console.log(underlines);
+console.log(navLinks);
 
 
-
-
-
-for (var i = 0; i < underlines.length; i++) {
-
+navLinks.forEach(function(element, index){
+  element.addEventListener("mouseenter", function() {
+    animateLine(index);
+  });
   
-  function animateLine() {
-    var width = 0;
-    var id = setInterval(function(){frame(i)}, 0.5);
+});
 
-    function frame(index) {
-      if (width >= 98) {
-        clearInterval(id);
-      } else if (width <= 98) {
-        width += 5;
-        // console.log(underlines[i]);
-        underlines[index].style.width = width + "%";
-      }
+navLinks.forEach(function (element, index) {
+  element.addEventListener("mouseleave", function () {
+    unanimateLine(index);
+  });
+});
+
+function animateLine(i) {
+  var width = 0;
+  var id = setInterval(function () {frame(i);}, 0.5);
+
+  function frame(i) {
+    if (width >= 98) {
+      clearInterval(id);
+    } else if (width <= 98) {
+      width += 5;
+      // console.log(underlines[i]);
+      underlines[i].style.width = width + "%";
     }
   }
+}
 
-  function unanimateLine(i) {
-      var width = 98;
-      var id = setInterval(frame, 0.5);
+function unanimateLine(i) {
+    var width = 98;
+    var id = setInterval(function() {frame(i);}, 0.5);
 
-      function frame() {
-        if (width <= 0) {
-          clearInterval(id);
-        } else if (width <= 98) {
-          width -= 2;
-          underlines[0].style.width = width + "%";
-        }
+    function frame(i) {
+      if (width <= 0) {
+        clearInterval(id);
+      } else if (width <= 98) {
+        width -= 2;
+        underlines[i].style.width = width + "%";
       }
-  }
+    }
+}
 
   
 
-  navLinks[i].addEventListener("mouseenter", animateLine);
-  navLinks[i].addEventListener("mouseleave", unanimateLine);
-}
 
 
-function changeColor(){
-  underlines[0].style.background = "red";
+// function changeColor(){
+//   underlines[0].style.background = "red";
 
-};
+// };
 
-navLinks[0].addEventListener("click", changeColor);
+// navLinks[0].addEventListener("click", changeColor);
 
 
--- starAnimation.js
+++ starAnimation.js
@@ -3,15 +3,89 @@
 
 
 var allStars = document.querySelectorAll(".hover-radius");
-var allSquares = document.getElementsByClassName("outer-square");
+var allSquares = document.querySelectorAll(".outer-square");
+
 
 console.log(allStars);
 console.log(allSquares);
 
-function shine(){
-    
-}
+allStars.forEach(function(star, index){
+    star.addEventListener("mouseenter", function(){
+        shine(index);
+        brightness(index);
+    });
+});
+
+allStars.forEach(function (star, index) {
+  star.addEventListener("mouseleave", function () {
+    fade(index);
+    unbrightness(index);
+  });
+});
 
 
-allStars[0].addEventListener("mouseenter", shine);
-allStars[0].addEventListener("mouseleave", fade);
+
+function shine(i){
+    var scale = 1;
+    var id = setInterval(function () {
+    frame(i);
+    }, 0.5);
+
+    function frame(i) {
+    if (scale >= 1.5) {
+        clearInterval(id);
+    } else if (scale <= 1.5) {
+        scale += 0.01;
+        allSquares[i].style.transform = "scale(" + scale + ")";
+    }
+    }
+};
+
+function fade(i) {
+  var scale = 1.5;
+  var id = setInterval(function () {
+    frame(i);
+  }, 10);
+
+  function frame(i) {
+    if (scale <= 1) {
+      clearInterval(id);
+    } else if (scale <= 1.5) {
+      scale -= 0.01;
+      allSquares[i].style.transform = "scale(" + scale + ")";
+    }
+  }
+};
+
+function brightness(i) {
+  var opacity = 0.2;
+  var id = setInterval(function () {
+    frame(i);
+  }, 0.5);
+
+  function frame(i) {
+    if (opacity >= 0.8) {
+      clearInterval(id);
+    } else if (opacity <= 0.8) {
+      opacity += 0.01;
+      allSquares[i].style.background = "rgba(255, 255, 255, " + opacity + ")";
+    }
+  }
+};
+
+
+function unbrightness(i) {
+  var opacity = 0.8;
+  var id = setInterval(function () {
+    frame(i);
+  }, 10);
+
+  function frame(i) {
+    if (opacity <= 0.2) {
+      clearInterval(id);
+    } else if (opacity <= 0.8) {
+      opacity -= 0.01;
+      allSquares[i].style.background = "rgba(255, 255, 255, " + opacity + ")";
+    }
+  }
+};
